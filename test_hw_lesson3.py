007c1e1ca304174c6e3a0af15f7bb5ef7a2300a0
oduvan4ik-7 <oduvashka7@gmail.com>
2021-12-21 11:26:02 +0300

    ex12: test_header_get

--- test_hw_lesson3.py
+++ test_hw_lesson3.py
@@ -22,3 +22,17 @@ def test_cookie():
     hw_cookie_value = response.cookies.get(hw_cookie_name_expected)
     assert hw_cookie_value == hw_cookie_value_expected, f"Cookie_value '{hw_cookie_value}' is not equal to '{hw_cookie_value_expected}'"
 
+
+# python -m pytest -s test_hw_lesson3.py -k head
+def test_header():
+
+    response = requests.get("https://playground.learnqa.ru/api/homework_header")
+    print(response.headers)
+    # ... 'x-secret-homework-header': 'Some secret value' ...
+
+    hw_header_name_expected = "x-secret-homework-header"
+    hw_header_value_expected = "Some secret value"
+
+    assert hw_header_name_expected in response.headers, f"No '{hw_header_name_expected}' in headers"
+    hw_header_value = response.headers.get(hw_header_name_expected)
+    assert hw_header_value == hw_header_value_expected, f"Header_value '{hw_header_value}' is not equal to '{hw_header_value_expected}'"
