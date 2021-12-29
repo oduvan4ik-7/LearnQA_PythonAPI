1ab5bb18d79a7c0e3d8a4e209a0bf835061c72b2
oduvan4ik-7 <oduvashka7@gmail.com>
2021-12-29 17:55:19 +0300

    ex16: test_get_user_details_auth_as_other_user

--- lib/assertions.py
+++ lib/assertions.py
@@ -42,6 +42,16 @@ class Assertions:
         assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it is present"
 
     @staticmethod
+    def assert_json_has_not_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it is present"
+
+    @staticmethod
     def assert_code_status(response: Response, expected_status_code):
         assert response.status_code == expected_status_code, f"Unexpected status code!" \
                                                              f" Expected:{expected_status_code}, " \
--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -5,6 +5,7 @@ from lib.my_requests import MyRequests
 
 # python -m pytest --alluredir=test_results/ tests/test_user_get.py
 class TestUserGet(BaseCase):
+
     def test_get_user_details_not_auth(self):
         response = MyRequests.get("/user/2")
 
@@ -25,10 +26,33 @@ class TestUserGet(BaseCase):
         user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
         response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
-                                 headers={"x-csrf-token":token},
-                                 cookies={"auth_sid": auth_sid})
+                                   headers={"x-csrf-token":token},
+                                   cookies={"auth_sid": auth_sid})
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_other_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        data_for_post = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=data_for_post)
+        Assertions.assert_code_status(response2, 200)
+        user_id = self.get_json_value(response2, "id")
+
+        response3 = MyRequests.get(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+        print(response3.content)
+
+        Assertions.assert_json_has_key(response3, "username")
+        Assertions.assert_json_has_not_keys(response3, ["email", "firstName", "lastName"])
 
 
